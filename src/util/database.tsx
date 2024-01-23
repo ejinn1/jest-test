70b98b72c735b3eb013063dfb32265deea71816b
ejinn1 <ejinn1@naver.com>
2024-01-23 18:07:29 +0900

    chore: mongoDB MongoClient 수정

--- src/util/database.tsx
+++ src/util/database.tsx
@@ -2,16 +2,16 @@ import { MongoClient } from "mongodb";
 
 const url =
   "mongodb+srv://ejinn1:dmlwls123@cluster0.wnzdedb.mongodb.net/?retryWrites=true&w=majority";
-const options: any = { useNewUrlParser: true };
+
 let connectDB: Promise<MongoClient>;
 
 if (process.env.NODE_ENV === "development") {
   if (!global._mongo) {
-    global._mongo = new MongoClient(url, options).connect();
+    global._mongo = new MongoClient(url).connect();
   }
   connectDB = global._mongo;
 } else {
-  connectDB = new MongoClient(url, options).connect();
+  connectDB = new MongoClient(url).connect();
 }
 
 export { connectDB };
